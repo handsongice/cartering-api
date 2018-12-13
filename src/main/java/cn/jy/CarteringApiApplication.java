d8d9e7286898fcc9d4755689743dbe3e4031e49d
handsongice <handsongice@163.com>
2018-12-13 21:17:38 +0800

    first commit

---
+++ src/main/java/cn/jy/CarteringApiApplication.java
@@ -0,0 +1,14 @@
+package cn.jy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarteringApiApplication.class, args);
+    }
+
+}
+
